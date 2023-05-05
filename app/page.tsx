336d3c4df53f500b318efa9f7caba7724b62216c
Thabish <kadertabish2@gmail.com>
2023-05-05 09:56:22 +0400

    echo.ts
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,40 @@
 import { UserInputs } from "@/components/UserInputs";
-import { exec } from "child_process";
+import { exec, spawn, fork } from "child_process";
 
-// For debugging
-// exec("echo how are you", (err, stdout, stderr) => {
-// 	if (err) {
-// 		console.error(`exec error: ${err}`);
-// 		return;
-// 	}
-// 	console.log(`stdout: ${stdout}`);
-// });
+// EXEC Example
+/* exec("echo how are you", (err, stdout, stderr) => {
+	if (err) {
+		console.error(`exec error: ${err}`);
+		return;
+	}
+	console.log(`stdout: ${stdout}`);
+}); */
+
+// SPAWN Example
+/* const child = spawn("echo", ["how", "are", "you"]);
+
+child.stdout.on("data", (data) => {
+	console.log(`stdout: ${data}`);
+});
+
+child.stderr.on("data", (data) => {
+	console.error(`stderr: ${data}`);
+});
+
+child.on("close", (code) => {
+	console.log(`child process exited with code ${code}`);
+}); */
+
+// FORK Example
+/* const child = fork("./echo.ts", ["how", "are", "you"]);
+
+child.on("message", (msg) => {
+	console.log(`stdout: ${msg}`);
+});
+
+child.on("close", (code) => {
+	console.log(`child process exited with code ${code}`);
+}); */
 
 export default function Home() {
 	return (
---
+++ echo.ts
@@ -0,0 +1,2 @@
+// Used for demonstrating how fork() works
+process.send?.(process.argv.slice(2).join(" "));
