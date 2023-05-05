0fc0f4e69aa774738be0c00efdb24bd8182d8b84
Thabish <kadertabish2@gmail.com>
2023-05-05 10:27:19 +0400

    readme
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Next Js + Python Automation
 
+## Child processes spawn(), fork() and exec()
+
 <img align="left" alt="next js" width="50px" style="padding-right:10px;" src="https://www.svgrepo.com/show/354113/nextjs-icon.svg" />
 <img align="left" alt="python" width="50px" style="padding-right:10px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png" />
 
@@ -15,6 +17,12 @@ A very small project to learn how to execute python script in Next js.
 
 ## Usecases for fork(), spawn() and exec()
 
--   fork() : used to create Node.js process to run a <ins>**sperate module**</ins> and can communicate with the parent process useing message channels
--   spawn() : used to run commands and scripts in a <ins>**new**</ins> instance of terminal
--   exec : useused to run commands and scripts in the <ins>**same**</ins> instance of terminald
+-   fork() : used to create Node.js process to run a <ins>**sperate module**</ins> and can communicate with the parent process useing message channels. Used for long running processes
+
+-   spawn() : used to run commands and scripts in a <ins>**new**</ins> instance of terminal. Used for long processes and large output
+
+-   exec : useused to run commands and scripts in the <ins>**same**</ins> instance of terminal. Used for short processes
+
+### Resources
+
+-   [Child processes](https://www.javatpoint.com/nodejs-child-process)
--- app/page.tsx
+++ app/page.tsx
@@ -2,39 +2,39 @@ import { UserInputs } from "@/components/UserInputs";
 import { exec, spawn, fork } from "child_process";
 
 // EXEC Example
-/* exec("echo how are you", (err, stdout, stderr) => {
-	if (err) {
-		console.error(`exec error: ${err}`);
-		return;
-	}
-	console.log(`stdout: ${stdout}`);
-}); */
+// exec("echo how are you", (err, stdout, stderr) => {
+// 	if (err) {
+// 		console.error(`exec error: ${err}`);
+// 		return;
+// 	}
+// 	console.log(`stdout: ${stdout}`);
+// });
 
 // SPAWN Example
-/* const child = spawn("echo", ["how", "are", "you"]);
+// const child = spawn("echo", ["how", "are", "you"]);
 
-child.stdout.on("data", (data) => {
-	console.log(`stdout: ${data}`);
-});
+// child.stdout.on("data", (data) => {
+// 	console.log(`stdout: ${data}`);
+// });
 
-child.stderr.on("data", (data) => {
-	console.error(`stderr: ${data}`);
-});
+// child.stderr.on("data", (data) => {
+// 	console.error(`stderr: ${data}`);
+// });
 
-child.on("close", (code) => {
-	console.log(`child process exited with code ${code}`);
-}); */
+// child.on("close", (code) => {
+// 	console.log(`child process exited with code ${code}`);
+// });
 
 // FORK Example
-/* const child = fork("./echo.ts", ["how", "are", "you"]);
+// const child = fork("./echo.ts", ["how", "are", "you"]);
 
-child.on("message", (msg) => {
-	console.log(`stdout: ${msg}`);
-});
+// child.on("message", (msg) => {
+// 	console.log(`stdout: ${msg}`);
+// });
 
-child.on("close", (code) => {
-	console.log(`child process exited with code ${code}`);
-}); */
+// child.on("close", (code) => {
+// 	console.log(`child process exited with code ${code}`);
+// });
 
 export default function Home() {
 	return (
